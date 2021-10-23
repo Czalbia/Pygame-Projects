f9f1cb08d5c84827c669cab9c621f7043c4160cf
Czalbia <58823176+Czalbia@users.noreply.github.com>
2021-10-23 23:41:34 +0200

    Update main.py

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -1,26 +1,20 @@
 import os
-import random
 
-
+board=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
 person='O'
 ai='X'
-currentPlayer=person
-isHuman = True
-
 
 def showBoard(place):
     print(f'''
-      _________
-      | {place[0]}|{place[1]}|{place[2]} |
-      | {place[3]}|{place[4]}|{place[5]} |
-      | {place[6]}|{place[7]}|{place[8]} |
-      |-------|''')
+  _________
+  | {place[0]}|{place[1]}|{place[2]} |
+  | {place[3]}|{place[4]}|{place[5]} |
+  | {place[6]}|{place[7]}|{place[8]} |
+  |-------|''')
 
+def someoneWin():
 
-def someoneWin(board):
-
-  
-  for i in (0,2):
+  for i in (0,2,1):
     if board[i] == board [i+3] == board[i+6] == person: return person
     if board[i] == board [i+3] == board[i+6] == ai: return ai
   
@@ -33,69 +27,68 @@ def someoneWin(board):
   if board[2] == board [4] == board[6] == person: return person
   if board[2] == board [4] == board[6] == ai: return ai
   
+  blanks =0
   for blank in board:
-    if blank == " ":
-      return None 
-  return " "
-  
+    if blank == ' ':
+      blanks+=1
+      
+  if blanks ==0: 
+    return 't'
 
-def minimax(board,depth,isMaximizing):
+  return None
+
+def minimax(depth,isMaximizing):
   scores ={
-    'X': -1,
+    'X': +1,
     'O':-1,
-    ' ': 0 
+    't': 0
   }
-  result= someoneWin(board)
+  result= someoneWin()
   if result != None:
     return scores[result]
     
   
   if isMaximizing:
-    bestScore= -100000
+    bestScore= -1000000
     for place in range( len(board)):
-      if board[place] == " ":
+      if board[place] == ' ':
         board[place]=ai
-        score = minimax(board,depth+1,False)
+        score = minimax(depth+1,False)
         board[place]=' '
         bestScore= max(score, bestScore)
   
     return bestScore
 
   else:
-    bestScore= 100000
+    bestScore= 1000000
     for place in range( len(board)):
-      if board[place] == " ":
+      if board[place] == ' ':
         board[place]=person
-        score = minimax(board,depth+1,True)
+        score = minimax(depth+1,True)
         board[place]=' '
         bestScore= min(score, bestScore)
   
     return bestScore
 
-  
-
-def bestMove(board):
+def bestMove():
   bestScore=-100000
+  move=0
   for place in range( len(board)):
     if board[place]==' ':
       board[place]= ai
-      score = minimax(board,0,False)
+      score = minimax(0,False)
       board[place]= ' '
       if score > bestScore:
         bestScore=score
-        bestMove=place
+        move=place
         
-  board[bestMove]=ai
-  currentPlayer=person
+  board[move]=ai
 
 def main():
-  
-  board=[" "," "," "," "," "," "," "," "," "]
-
 
   print(f'''
-        Your Mark: X
-        You Start!
+        You'r Mark: O
+        You'r Second!
         Please choose where you want to set your mark:
     _________
     | 0|1|2 |
@@ -103,31 +96,35 @@ def main():
     | 6|7|8 |
     |-------|
         ''')
-  
+  iter =1 
   while True:
-    if someoneWin(board) == None:
-      a =0 
-    elif someoneWin(board)!=" ":
-        print(someoneWin(board)," wins!\n")
+    
+    if someoneWin() == None:
+      pass
+    elif someoneWin()!='t':
+        print(someoneWin()," wins!\n")
         break
-    elif someoneWin(board)==" ":
+    elif someoneWin()=='t':
         print("Tie!\n")
         break
     
-
-    if currentPlayer==person:
+    if iter %2==0:
       place = int(input("Your move:"))
-      if board[place] ==" ":
+      if board[place] ==' ':
         board[place] = person
-        currentPlayer=ai
-        bestMove(board)
+      else: continue 
+    else: bestMove()
     
-      
- 
     os.system("cls")
     print(board)
-    
+    print('''
+  _________
+  | 0|1|2 |
+  | 3|4|5 |
+  | 6|7|8 |
+  |-------|''')
     showBoard(board)
+    iter +=1
 
 
 if __name__ == '__main__':
