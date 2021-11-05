8417528289aad2179bd3b3e23f311cade6e94c19
Czalbia <58823176+Czalbia@users.noreply.github.com>
2021-11-05 23:16:33 +0100

    Update main.py

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -1,8 +1,8 @@
 import os
 import random
+import visual
 
 board=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
-
 if random.randint(1,2)==1:
       person = 'X'
       ai = 'O'
@@ -10,30 +10,57 @@ else:
   ai='X'
   person = 'O'
 
+def checkPosition():
+    while True:
+        pos=visual.pygame.mouse.get_pos()
+        x = pos[0]
+        y = pos[1]
+        for event in visual.pygame.event.get():
+            if event.type == visual.pygame.QUIT:
+                exit()
+            if event.type == visual.pygame.MOUSEBUTTONDOWN:
+                if x>0 and x <240 and y<240:
+                    return 0
+                if x>240 and x<480 and y<240:
+                    return 1
+                if x>480 and x<720 and y<240:
+                    return 2
+                if x>0 and x <240 and y>240 and y<480:
+                    return 3
+                if x>240 and x<480 and y>240 and y<480:
+                    return 4
+                if x>480 and x<720 and y>240 and y<480:
+                    return 5
 
-
-def showBoard(place):
-    print(f'''
-  _________
-  | {place[0]}|{place[1]}|{place[2]} |
-  | {place[3]}|{place[4]}|{place[5]} |
-  | {place[6]}|{place[7]}|{place[8]} |
-  |-------|''')
+                if x>0 and x <240 and y>480 and y<720:
+                    return 6
+                if x>240 and x<480 and y>480 and y<720:
+                    return 7
+                if x>480 and x<720 and y>480 and y<720:
+                    return 8
 
 def someoneWin():
 
   for i in (0,2,1):
-    if board[i] == board [i+3] == board[i+6] == person: return person
-    if board[i] == board [i+3] == board[i+6] == ai: return ai
+    if board[i] == board [i+3] == board[i+6] == person: 
+      return person
+    if board[i] == board [i+3] == board[i+6] == ai: 
+      return ai
   
   for i in (0,6,3):
-    if board[i] == board [i+1] == board[i+2] == person: return person
-    if board[i] == board [i+1] == board[i+2] == ai: return ai
+    if board[i] == board [i+1] == board[i+2] == person: 
+      return person
+    if board[i] == board [i+1] == board[i+2] == ai: 
+      return ai
 
-  if board[0] == board [4] == board[8] == person: return person
-  if board[0] == board [4] == board[8] == ai: return ai
-  if board[2] == board [4] == board[6] == person: return person
-  if board[2] == board [4] == board[6] == ai: return ai
+  if board[0] == board [4] == board[8] == person: 
+    return person
+  if board[0] == board [4] == board[8] == ai: 
+    return ai
+  if board[2] == board [4] == board[6] == person: 
+    return person
+  if board[2] == board [4] == board[6] == ai: 
+    return ai
   
   blanks =0
   for blank in board:
@@ -41,8 +68,10 @@ def someoneWin():
       blanks+=1
       
   if blanks ==0: 
+    
     return 't'
 
+  
   return None
 
 def minimax(depth,isMaximizing):
@@ -60,6 +89,7 @@ def minimax(depth,isMaximizing):
     }
   result= someoneWin()
   if result != None:
+    
     return scores[result]
     
   
@@ -72,6 +102,7 @@ def minimax(depth,isMaximizing):
         board[place]=' '
         bestScore= max(score, bestScore)
   
+    
     return bestScore
 
   else:
@@ -83,6 +114,7 @@ def minimax(depth,isMaximizing):
         board[place]=' '
         bestScore= min(score, bestScore)
   
+    
     return bestScore
 
 def bestMove():
@@ -100,68 +132,42 @@ def bestMove():
   board[move]=ai
 
 def main():
-  if person == 'X':
-        print(f'''
-        Your Mark: {person}
-        You are going First!
-        Please choose where you want to set your mark:
-    _________
-    | 0|1|2 |
-    | 3|4|5 |
-    | 6|7|8 |
-    |-------|
-        ''')
-  else: print(f'''
-        Your Mark: {person}
-        You are going Second!
-        Please choose where you want to set your mark:
-    _________
-    | 0|1|2 |
-    | 3|4|5 |
-    | 6|7|8 |
-    |-------|
-        ''')
   iter =1 
+  if person=='X':
+    visual.showText("You start!")
+  else:
+    visual.showText("Computer starts!")
   while True:
-    
-    if someoneWin() == None:
-      pass
-    elif someoneWin()!='t':
-        print(someoneWin()," wins!\n")
+    visual.main(board)
+    result = someoneWin()
+    if result==person:
+        visual.showWinner(person)
         break
-    elif someoneWin()=='t':
-        print("Tie!\n")
+    if result==ai:
+        visual.showWinner(ai)
+        break
+    if result=='t':
+        visual.showTie()
         break
 
     if person == 'X':
       if iter %2!=0:
-        place = int(input("Your move:"))
+        place = checkPosition()
         if board[place] ==' ':
           board[place] = person
         else: continue   
       else:
         bestMove()
-
     if person == 'O':
       if iter %2==0:
-        place = int(input("Your move:"))
+        place = checkPosition()
         if board[place] ==' ':
           board[place] = person
         else: continue 
       else:
         bestMove()
-    
-    os.system("clear")
-    print(board)
-    print('''
-  _________
-  | 0|1|2 |
-  | 3|4|5 |
-  | 6|7|8 |
-  |-------|''')
-    showBoard(board)
-    iter +=1
 
+    iter +=1
 
 if __name__ == '__main__':
   main()
